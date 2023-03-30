0f98f9f40955b969c0d806a7ab900b1c8be5101f
Martha <martha.maina@student.moringaschool.com>
2023-03-30 07:35:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function saturdayFun(day = "This Saturday", activity = "roller-skate") {
-    console.log(`${day} I want to ${activity}`);
-}
-saturdayFun();
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
 
+  function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
 
-
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); 
+  // Output: You are !!!a dedicated programmer!!!
+  console.log(encouragingPromptFunction()); 
+  // Output: You are !!!special!!!
